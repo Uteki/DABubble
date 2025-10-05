514212d8942f97f4bf0b787743b3ad99ac94ca73
Daniel Tran <68958503+Uteki@users.noreply.github.com>
2025-10-05 23:56:59 +0200

    Dashboard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register/register.component';
 import { AvatarComponent } from './login/avatar/avatar.component';
 import { HeaderComponent } from './shared/header/header.component';
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'avatar', component: AvatarComponent },
-    { path: 'header', component: HeaderComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'header', component: HeaderComponent },
   { path: '**', redirectTo: '' },
 ];
