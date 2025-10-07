8f776f545bfef41d7f81c5b7921b741ddfa5c9cf
EmreIscan-47 <iscan.connect@outlook.de>
2025-10-07 11:15:49 +0200

    Add Header Profile and edit Profile

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -9,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-username:string = "Frederik Beck"
+username:string = "Frederik Beck";
+useremail:string = " fred.back@email.com ";
+edit: boolean = false;
+
+
+toggleDropdown() {
+  const dropdown = document.getElementById("dropdownMenu");
+  if (dropdown) {
+    dropdown.classList.toggle("no-display");
+  } 
+}
+
+stopPropagation(event: Event) {
+  event.stopPropagation();
+}
+
+toggleProfile() {
+  this.edit = false;
+  console.log("yo");
+  
+  const profileMenu = document.getElementById("profile-menu");
+  if (profileMenu) {
+    profileMenu.classList.toggle("no-display");
+  } 
+}
+
+editProfile() {
+  this.edit = true;
+}
 }
