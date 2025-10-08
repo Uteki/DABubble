8127a9ec671535458d60cb564ea000d4face56af
Daniel Tran <68958503+Uteki@users.noreply.github.com>
2025-10-08 14:51:08 +0200

    Chat and Thread

--- src/app/dashboard/chat/chat.component.ts
+++ src/app/dashboard/chat/chat.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  message: string = '';
 
+  sendMessage(event: Event): void {
+    event.preventDefault();
+    if (!this.message.trim()) return;
+    console.log('Send:', this.message);
+    this.message = '';
+  }
 }
--- src/app/dashboard/thread/thread.component.ts
+++ src/app/dashboard/thread/thread.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [],
+    imports: [
+        FormsModule
+    ],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
 export class ThreadComponent {
+  message: string = '';
 
+  sendMessage(event: Event): void {
+    event.preventDefault();
+    if (!this.message.trim()) return;
+    console.log('Send:', this.message);
+    this.message = '';
+  }
 }
