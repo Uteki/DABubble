14a2a1bd47fbf098dcf19d959f1b80b10d4a0165
EmreIscan-47 <iscan.connect@outlook.de>
2025-10-10 11:41:15 +0200

    Add overlays to chat section

--- src/app/dashboard/chat/chat.component.ts
+++ src/app/dashboard/chat/chat.component.ts
@@ -1,19 +1,26 @@
 import {Component, OnInit} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {ChatService} from "../../chat.service";
+import { NgClass } from '@angular/common';
+
 
 @Component({
   selector: 'app-chat',
   standalone: true,
   imports: [
-    FormsModule
-  ],
+    FormsModule,
+    NgClass
+],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent implements OnInit {
   messages: any[] = [];
   messageText: string = '';
+  overlayActivated: boolean = false;
+  channelOverlay: boolean = false;
+  viewMemberOverlay: boolean = false;
+  addMemberOverlay: boolean = false;
 
   constructor(private chatService: ChatService) {}
 
@@ -34,5 +41,16 @@ export class ChatComponent implements OnInit {
     });
 
     this.messageText = '';
+  }
+
+  overlayFunction(darkOverlay: boolean, overlay: string, overlayBoolean: boolean ) {
+    this.overlayActivated = darkOverlay;
+    if (overlay == "Entwicklung") {
+      this.channelOverlay = overlayBoolean;
+    } else if (overlay == "Mitglieder") {
+      this.viewMemberOverlay = overlayBoolean;
+    } else if (overlay == "Hinzufügen") {
+      this.addMemberOverlay == overlayBoolean;
+    }
   }
 }
