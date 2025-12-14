b14851d5008d116ddffe1b232b1d98ed09b928e1
Daniel Tran <68958503+Uteki@users.noreply.github.com>
2025-12-14 23:51:57 +0100

    Test data and overall message function to send on multiply recipients

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -64,9 +64,26 @@ export class ChatService {
     return addDoc(messagesRef, message);
   }
 
-  sendBroadcastMessage(recipients: any, message: {uid: string, text: string; user: string; timestamp: number; reaction: any }) {
-    // if (recipients[test] = mail || person) {}
-    // if (recipients[test] = channel) {}
+  async sendBroadcastMessage(recipients: any, message: {uid: string, text: string; user: string; timestamp: number; reaction: any }) {
+    for (const r of recipients) {
+      switch (r.type) {
+        case 'channel':
+          await this.sendMessage(r.channelId, message);
+          break;
+
+        case 'user':
+          await this.sendWhisperMessage(r.partnerChat, message);
+          break;
+
+        case 'mail':
+          // await this.sendToMail(r.mail, message);
+          break;
+      }
+    }
+  }
+
+  private sendToMail(mail: string, message: any) {
+    console.log('Send mail to', mail, message);
   }
 
   async messageThreaded(channelId: string, threadId: string, amount: number, last: number ) {
